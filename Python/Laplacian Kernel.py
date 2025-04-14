bc8b92b87e8941ec794bc7adc5031121b8eaeaec
Raian Rashid <rimonraian2@gmail.com>
2025-04-15 01:16:48 +0600

    laplaciankernelPython

--- Python/Laplacian Kernel.py
+++ Python/Laplacian Kernel.py
@@ -10,7 +10,7 @@ laplacian_kernel = -4 * np.ones((kernel_size, kernel_size))
 laplacian_kernel += np.eye(kernel_size) * (kernel_size**2 - 1)
 
 # Read the image
-input_image = cv2.imread('D:\\Academics\Semesters\\4-1\\Lab\\Digital Image Processing Laboratory\\Programs\\Lab4\\stray.jpg', cv2.IMREAD_GRAYSCALE)
+input_image = cv2.imread('Enter the path to the image here', cv2.IMREAD_GRAYSCALE)
 
 # Apply convolution
 filtered_image = cv2.filter2D(src=input_image.astype(np.float32), ddepth=-1, kernel=laplacian_kernel)
