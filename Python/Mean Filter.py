fb16aa5fa6e92d487e5c6f1ca8209ef486603613
RaianRashidRimon <rimonraian2@gmail.com>
2025-04-17 21:49:23 +0600

    meanfilterpython

---
+++ Python/Mean Filter.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import convolve
+import imageio.v2 as imageio
+
+
+img = imageio.imread('D:\\Academics\\Semesters\\4-1\\Lab\\Digital Image Processing Laboratory\\Programs\\Lab4\\noisy_image.jpg')
+img = img.astype(np.uint8)
+
+filter_size = 6
+kernel = np.ones((filter_size, filter_size), dtype=np.float32) / (filter_size ** 2)
+
+filtered_img = np.zeros_like(img)
+for c in range(img.shape[2]):
+    filtered_img[:, :, c] = convolve(img[:, :, c], kernel, mode='nearest')
+
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.imshow(img)
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(filtered_img)
+plt.title('Filtered Image with Mean Filter')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+
