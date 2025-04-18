3028f3daa884d48f4925820a4b6337cc4fff047d
RaianRashidRimon <rimonraian2@gmail.com>
2025-04-18 22:06:32 +0600

    median filter matlab

---
+++ MATLAB/Median Filter.m
@@ -0,0 +1,15 @@
+inputImage = imread('/MATLAB Drive/noisy_image.jpg');
+filterSize = 3;
+[rows, cols, numChannels] = size(inputImage);
+filteredImage = zeros(rows, cols, numChannels, 'like', inputImage);
+for channel = 1:numChannels
+    filteredImage(:, :, channel) = medfilt2(inputImage(:, :, channel), [filterSize filterSize]);
+end
+figure('Position', [100, 100, 600, 800]); 
+subplot(2, 1, 1);
+imshow(inputImage);
+title('Original Image');
+subplot(2, 1, 2);
+imshow(filteredImage);
+title('Filtered Image of Median Filter');
+imwrite(filteredImage, 'filtered_image.jpg');
