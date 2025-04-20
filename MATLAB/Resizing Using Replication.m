cd66faa99faebe3910dc5dac68e9662598d569ca
RaianRashidRimon <rimonraian2@gmail.com>
2025-04-21 01:24:24 +0600

    resizing using replication matlab

---
+++ MATLAB/Resizing Using Replication.m
@@ -0,0 +1,25 @@
+img = imread('/MATLAB Drive/stray.jpg');
+newRows = 500; 
+newCols = 500; 
+[originalRows, originalCols, numChannels] = size(img);
+resizedImg = uint8(zeros(newRows, newCols, numChannels));
+rowScale = originalRows / newRows;
+colScale = originalCols / newCols;
+for r = 1:newRows
+    for c = 1:newCols
+        origRow = round((r - 0.5) * rowScale + 0.5);
+        origCol = round((c - 0.5) * colScale + 0.5);
+        
+        origRow = max(1, min(originalRows, origRow));
+        origCol = max(1, min(originalCols, origCol));
+        
+        resizedImg(r, c, :) = img(origRow, origCol, :); % Keep RGB channels
+    end
+end
+figure('Position', [100, 100, 600, 800]);
+subplot(2, 1, 1);
+imshow(img);
+title('Original Image');
+subplot(2, 1, 2);
+imshow(resizedImg);
+title('Resized Image using Replication Method');
