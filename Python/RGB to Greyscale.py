685aeacdfb5f0a74cee67856e6db193d62910940
RaianRashidRimon <rimonraian2@gmail.com>
2025-04-24 22:43:13 +0600

    rgb to grey python

---
+++ Python/RGB to Greyscale.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio.v2 as imageio
+from skimage.color import rgb2gray
+
+img = imageio.imread('C:\\Users\\Admin\\OneDrive\\Desktop\\mario.jpeg')
+grayscale_img = rgb2gray(img)
+
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.imshow(img)
+plt.title('Original RGB Image', fontsize=12)
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(grayscale_img, cmap='gray')
+plt.title('Grayscale Image', fontsize=12)
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
