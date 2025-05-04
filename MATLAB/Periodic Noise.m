ff1fd694cc8bc92b23efcaee2ec0c32d2a98b9ff
Raian Rashid <rimonraian2@gmail.com>
2025-05-05 01:08:41 +0600

    Update Periodic Noise.m

--- MATLAB/Periodic Noise.m
+++ MATLAB/Periodic Noise.m
@@ -1,28 +1,20 @@
 originalImage = imread('Enter you image path here');
 originalImage = im2double(originalImage); 
-
 [rows, cols, ~] = size(originalImage);
 [x, y] = meshgrid(1:cols, 1:rows);
-
 frequency = 20; 
 periodicNoise = 0.1 * sin(2 * pi * frequency * x / cols) + ...
                 0.1 * sin(2 * pi * frequency * y / rows);
 
 noisyImage = originalImage;
-
 for channel = 1:size(originalImage, 3)
     noisyImage(:, :, channel) = originalImage(:, :, channel) + periodicNoise;
 end
-
-% Clip the noisy image to [0, 1]
 noisyImage = min(max(noisyImage, 0), 1);
-
-% Display original and noisy images
 figure;
 subplot(1, 2, 1);
 imshow(originalImage);
 title('Original RGB Image');
-
 subplot(1, 2, 2);
 imshow(noisyImage);
 title('Image with Periodic Noise');
